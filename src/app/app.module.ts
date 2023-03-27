028f67511652c7fc7e16454307aaa39d37ee2eea
AbdelrhmanBadran <AbdelrhmanBadran>
2023-03-27 06:46:02 +0200

    appp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
@@ -29,4 +28,6 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+}
